46758a7c0f6bfe9db1f2fdf1040817ee8ec5d343
Socheat Sok <socheatsok78@gmail.com>
2025-05-20 15:24:49 +0700

    Update nix-darwin/flake.nix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,9 +2,13 @@
   description = "Socheat's darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
