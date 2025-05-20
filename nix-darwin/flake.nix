401f55cb258b79682ce76ddf1c0d1d722c8aec5d
Socheat Sok <socheatsok78@gmail.com>
2025-05-20 15:28:00 +0700

    Add home-manager to nix-darwin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Socheat's darwin system flake";
-
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
@@ -9,13 +7,17 @@
       url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
   outputs =
     inputs@{
       self,
       nix-darwin,
       nixpkgs,
+      ...
     }:
     let
       configuration =
