9fcd242c972ec980b1f97508c8488682ebfd7477
Socheat Sok <socheatsok78@gmail.com>
2025-05-20 17:09:13 +0700

    Update nix-darwin/home.nix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -10,13 +10,13 @@
     pkgs.htop
   ];
 
-  # This value determines the Home Manager release that your configuration is 
-  # compatible with. This helps avoid breakage when a new Home Manager release 
-  # introduces backwards incompatible changes. 
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
   #
-  # You should not change this value, even if you update Home Manager. If you do 
-  # want to update the value, then make sure to first check the Home Manager 
-  # release notes. 
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # Let Home Manager install and manage itself.
